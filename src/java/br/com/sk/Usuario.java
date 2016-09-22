cfd0108bfa6d3377f4cbf59909651da6073e0238
Odirlei <Odirlei@Odirlei>
2016-09-21 21:49:46 -0300

    Criação da Classe do Usuario

---
+++ src/java/br/com/sk/Usuario.java
@@ -0,0 +1,21 @@
+
+package br.com.sk;
+
+
+/**
+ *
+ * @author Odirlei
+ */
+public class Usuario {
+    private int id;
+    private String nome;
+    private String sobreNome;
+    private String eamil;
+    private String celular;
+    private String senha;
+    private String login;
+    private String uf;
+    private String cidade;
+    private String facebook;
+        
+}
